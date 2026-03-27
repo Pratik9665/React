42e3b8f6f16c9f7d9127fb88d30be7e4e9d17beb
Harshal Kathar <harshalkkathar788@gmail.com>
2026-03-27 21:29:53 +0530

    added map function in photo gallery

--- src/components/Photo Gallery/CardTab.jsx
+++ src/components/Photo Gallery/CardTab.jsx
@@ -2,6 +2,35 @@ import React from "react"
 import Card from "./Card"
 
 const CardTab = () => {
+
+   const cities = [
+    {
+      url: "https://alumni.jnec.org/storage//albums/Alumni%20Pre-meet%20Delhi/EE668XA8sef4CufECLmi3Hx9SLdbQGaUQmkw4hUv.png",
+      location: "Delhi",
+      path: "/gallery/delhi",
+      photoCount: 8
+    },
+    {
+      url: "https://alumni.jnec.org/storage//albums/Mumbai/iaSGKlyajeYrpOilD1Ai4ekiNTExN7RxqnEjZ1Um.jpg",
+      location: "Mumbai",
+      path: "/gallery/mumbai",
+      photoCount: 10
+    },
+    {
+      url: "https://alumni.jnec.org/storage//albums/Aurangabad/lmAs4ildNH97fvLDE0F9p1vBq5TkiqxjSZ0VUEho.jpg",
+      location: "Chh.Sambhajinagar",
+      path: "/gallery/sambhajinagar",
+      photoCount: 6
+    },
+    {
+      url: "https://alumni.jnec.org/storage//albums/Pune/ECA67uqGvdxbfa6iGxXUuIHZABLvTgoSANOMDuBZ.jpg",
+      location: "Pune",
+      path: "/gallery/pune",
+      photoCount: 12
+    }
+  ]
+
+  
   return (
     <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
       
@@ -11,31 +40,16 @@ const CardTab = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
         
-        <Card 
-          url="https://alumni.jnec.org/storage//albums/Alumni%20Pre-meet%20Delhi/EE668XA8sef4CufECLmi3Hx9SLdbQGaUQmkw4hUv.png" 
-          location="Delhi" 
-          path="/gallery/delhi"
-        />
-
-        <Card 
-          url="https://alumni.jnec.org/storage//albums/Mumbai/iaSGKlyajeYrpOilD1Ai4ekiNTExN7RxqnEjZ1Um.jpg" 
-          location="Mumbai" 
-          path="/gallery/mumbai"
-          photoCount= {10}
-        />
-
-        <Card 
-          url="https://alumni.jnec.org/storage//albums/Aurangabad/lmAs4ildNH97fvLDE0F9p1vBq5TkiqxjSZ0VUEho.jpg" 
-          location="Chh.Sambhajinagar" 
-          path="/gallery/sambhajinagar"
-
-        />
-
-        <Card 
-          url="https://alumni.jnec.org/storage//albums/Pune/ECA67uqGvdxbfa6iGxXUuIHZABLvTgoSANOMDuBZ.jpg" 
-          location="Pune" 
-          path="/gallery/pune"
-        />
+    {
+          cities.map((city) => (
+          <Card
+            url={city.url}
+            location={city.location}
+            path={city.path}
+            photoCount={city.photoCount}
+          />
+        ))
+    }
 
       </div>
     </div>
